01226bf6f2df6f099eb7f3f0276ef1346a7e45aa
Eric Schultz <eschultz@prplfoundation.org>
2016-05-09 13:08:15 -0500

    add support for DD
    Signed-off-by: Eric Schultz <eschultz@prplfoundation.org>

--- build.sh
+++ build.sh
@@ -10,34 +10,45 @@ done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 function clean {
-  [ -d $DIR/build ] && $( cd $DIR/build; make clean ) && cd $DIR
+  [ -d $DIR/$1 ] && $( cd $DIR/$1; make clean ) && cd $DIR
 }
 
 function dirclean {
-  [ -d $DIR/build ] && $( cd $DIR/build; make dirclean )  && cd $DIR
+  [ -d $DIR/$1 ] && $( cd $DIR/$1; make dirclean )  && cd $DIR
 }
 
-if [ "$1" == "cc" ]; then
-  [ "$2" == "clean" ] && clean
-
+function run_build {
+  VERSION=$1
+  GIT_REPO=$2
+  [ "$3" == "clean" ] && clean $VERSION.build
   cd $DIR
-  git -C build pull --ff-only || git clone https://git.openwrt.org/15.05/openwrt.git build
-  rm build/feeds.conf
-  echo "src-link boardcoop $DIR/cc-feed"|cat - cc.feeds.conf > /tmp/out && mv /tmp/out build/feeds.conf
-  rm build/.config
-  cp cc.config build/.config
-  cd build
+  git -C $VERSION.build pull --ff-only || git clone $GIT_REPO $VERSION.build
+  rm $VERSION.build/feeds.conf
+  echo "src-link boardcoop $DIR/$VERSION-feed"|cat - $VERSION.feeds.conf > /tmp/out && mv /tmp/out $VERSION.build/feeds.conf
+  rm $VERSION.build/.config
+  cp $VERSION.config $VERSION.build/.config
+  cd $VERSION.build
   scripts/feeds update
   scripts/feeds install -a
   make defconfig
   make V=s
   cd $DIR
-elif [ "$1" == "dd" ]; then
- echo "dd"
-elif [ "$1" == "update-cc" ] ; then
-  cd $DIR/build
-  scripts/diffconfig.sh >> $DIR/cc.config
+}
+
+function run_update {
+  cd $DIR/$1.build
+  scripts/diffconfig.sh >> $DIR/$1.config
   cd $DIR
+}
+
+if [ "$1" == "cc" ]; then
+  run_build 'cc' 'https://git.openwrt.org/15.05/openwrt.git' $2
+elif [ "$1" == "dd" ]; then
+ run_build 'dd' 'https://git.openwrt.org/openwrt.git' $2
+elif [ "$1" == "update-cc" ] ; then
+  run_update 'cc'
+elif [ "$1" == "update-dd" ] ; then
+  run_update 'dd'
 else
   echo "You must select cc or dd"
 fi
